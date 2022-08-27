4feeff6a86cdb221298392ea0956cc71bcacd3fe
LuisDiegoAndrade <luiss@LAPTOP-IBQ6BV6O.localdomain>
2022-08-26 22:34:05 -0700

    Added working script that executes raw SQL statement

---
+++ query.py
@@ -0,0 +1,27 @@
+'''
+This script tests executing raw SQL statements in SQLAlchemy.
+We take advantage of SQLAlchemy's structuring of the queries, but we strictly only use raw query statements for the sake of the course and its goals.
+'''
+
+'''
+Data comes from Kaggle.com
+'''
+
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+#Change this path when running on your system if needed. Database is in the current working directory.
+engine = sqlalchemy.create_engine('sqlite:///cve')
+Session = scoped_session(sessionmaker(bind=engine))
+
+s = Session()
+
+# run query against 'cve-vendors-products' table that returns all rows where the vendor
+## is google
+
+# Escape "-" character in table name by wrapping table name in back ticks
+result = s.execute('SELECT * FROM `cve-vendors-products` WHERE vendor = :val', {'val': "google"})
+
+# iterate through results
+for item in result:
+    print(item)
